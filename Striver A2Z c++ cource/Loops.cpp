f023af410266e77c9b9979cc7a24114511251a4b
Sakshi bagul <bagulsakshi29@gmail.com>
2024-12-24 12:02:25 +0000

    container config done

---
+++ Striver A2Z c++ cource/Loops.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+// For loop
+// int n=5;
+//     for(int i=0;i<n;i++){
+//         cout<<"Hello Sakshi  "<<i<<endl;
+//     }
+
+// while loop
+
+// int n=6;
+// int i;
+// while(i<n){
+//    cout<<"Hello Sakshi  "<<i<<endl;
+//    i++;
+// }
+
+
+// Do while loop
+
+int i;
+int n=7;
+do{
+    cout<<"Hello Sakshi  "<<i<<endl;
+     i++;
+}while(i<n);
+    return 0;
+
+}

31da68677cfa9eeee0b8a6d6553cebb29465822a
Sakshi bagul <bagulsakshi29@gmail.com>
2024-12-24 11:00:10 +0000

    container config done

---
+++ Striver A2Z c++ cource/Conditional.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+ int age;
+ cout<<"Enter age"<<endl;
+cin>>age;
+    if(age>=18){
+        cout<<"Adult";
+    }else{
+        cout<<"Not Adult";
+    }
+    return 0;
+}

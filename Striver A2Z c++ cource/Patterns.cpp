c92891287078d6edd469357111db02dbaf9d3510
Sakshi bagul <bagulsakshi29@gmail.com>
2024-12-25 11:49:24 +0000

    container config done

--- Striver A2Z c++ cource/Patterns.cpp
+++ Striver A2Z c++ cource/Patterns.cpp
@@ -5,14 +5,14 @@ using namespace std;
 // ****
 // ****
 // ****
-void printPattern1(){
-    for(int i=0;i<4;i++){
-        for(int j=0; j<4;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-}
+// void printPattern1(){
+//     for(int i=0;i<4;i++){
+//         for(int j=0; j<4;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
 
 //for n number of Rows and columns
 // void printPattern1(int n){
@@ -25,8 +25,116 @@ void printPattern1(){
 // }
 
 
-int main(){
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+
+// void printPattern2(int n){
+//     for (int i=0;i<n;i++){
+//         for (int j=0;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+// 1 2 3 4 5 6 
+// void printPattern3(int n){
+//     for (int i=1;i<n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
+// 6 6 6 6 6 6 
+// void printPattern4(int n){
+//     for (int i=1;i<n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// * * * * * * 
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+// void printPattern5(int n){
+//     for (int i=1;i<n;i++){
+//         for(int j=1;j<n-i+1;j++){
+//             cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// 1 2 3 4 5 6 7 
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+// void printPattern6(int n){
+//     for (int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i+1;j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//     **    
+//    ****   
+//   ******  
+//  ******** 
+// **********
+void printPattern7(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<=2*i+1;j++){
+            cout<<"*";
+        }
+         for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+
+        cout<<endl;
+
+
+    }
+}
+
+int main() {
     // printPattern1();
     //  printPattern1(7);
-    return 0;
+    // printPattern2(7);
+    //  printPattern3(7);
+    // printPattern4(7);
+    // printPattern5(7);
+    // printPattern6(7);
+    printPattern7(5);
+    return 0 ;
 }

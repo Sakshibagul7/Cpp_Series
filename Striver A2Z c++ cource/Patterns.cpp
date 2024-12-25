3636d37cb6c3e70915266adfc26996c9f479127d
Sakshi bagul <bagulsakshi29@gmail.com>
2024-12-25 05:57:31 +0000

    container config done

---
+++ Striver A2Z c++ cource/Patterns.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// ****
+// ****
+// ****
+// ****
+void printPattern1(){
+    for(int i=0;i<4;i++){
+        for(int j=0; j<4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+//for n number of Rows and columns
+// void printPattern1(int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+int main(){
+    // printPattern1();
+    //  printPattern1(7);
+    return 0;
+}

56c7c904d7d8f5d9c5d0dce7d84afe766949975f
Sakshi bagul <bagulsakshi29@gmail.com>
2024-12-31 13:11:11 +0000

    container config done

---
+++ Striver A2Z c++ cource/Recursion.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//  This is with BASE CONDITION
+
+// int cnt=0;
+// void print(){
+//     if(cnt==3) return;
+//     cout<<cnt<<endl;
+//     cnt++;
+//     print();
+// }
+// int main(){
+//     #ifndef ONLINE_JUDGE
+//     freopen("input.txt","r",stdin);
+//     freopen("output.txt","w", stdin);
+//     #endif
+
+//     print();
+//     return 0;
+// }
+
+
+// Print n numbers using recursion
+// int n,i;
+// void f(int i,int n){
+//     if(i>n) return;
+//     cout<<("sakhi");
+//     f(i+1,n);
+//     }
+//     int main(){
+//         int n;
+//         cin>>n;
+//         f(i,n);
+
+//         return 0;
+//     }
+
+
+// print numbers form 1 to N
+
+// int n,i;
+
+//  void f( int i, int n){
+//     if(i>n){
+//         return;
+//           }
+//         cout<<i<<" ";
+//      f(i+1,n);
+//  }
+//   int main (){
+//     cout<<"Enter value for n";
+//     int n;
+//     cin>>n;
+//     f(i,n);
+
+//   }
+
+
+// print numbers form 1 to N in reverse order
+// int n,i;
+// void f(int i, int n){
+//     if(i<1){
+//         return; }
+//         cout<<i<<" ";
+    
+//     f(i-1,n);
+// }
+// int main(){
+//     cout<<"Enter value for n"; 
+//     cin>>n;
+//     f(n,n);
+//     return 0;
+// }
+
+
+// Print numbers from  1 to N using backtracking
+
+// int n,i;
+// void f(int i,int n){
+//     if(i<1) 
+//     return;
+//     f(i-1,n);
+//     cout<<i<<" ";
+// }
+// int main(){
+//     cout<<"Enter value for n"; 
+//     cin>>n;
+//     f(n,n);
+//     return 0;
+// }
+
+
+int n,i;
+void f(int i, int n){
+    
+}

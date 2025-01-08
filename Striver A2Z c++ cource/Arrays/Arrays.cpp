50a11f33f0cd894c984e48f37c48df23957f014b
Sakshi bagul <bagulsakshi29@gmail.com>
2025-01-08 08:55:00 +0000

    container config done

---
+++ Striver A2Z c++ cource/Arrays/Arrays.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={11,2,37,4,5};
+    // cout<<arr[0]<<endl;
+
+    //loops in arrays
+
+    int n=sizeof(arr);
+
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
